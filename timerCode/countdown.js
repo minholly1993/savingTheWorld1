ce3adeca2b803f43c846276478be3e73273708e2
minholly1993 <58905896+minholly1993@users.noreply.github.com>
2023-03-20 21:20:19 +0800

    Update countdown.js

--- timerCode/countdown.js
+++ timerCode/countdown.js
@@ -1,16 +1,27 @@
+let timerInterval
+let totalSeconds1 = 0
+let timepass = 0
+let stopt = false
 function startTimer() {
+  stopt = false
   let hoursInput = document.getElementById("hours").value
   let minutesInput = document.getElementById("minutes").value
   let secondsInput = document.getElementById("seconds").value
 
   totalSeconds1 = (hoursInput * 3600) + (minutesInput * 60) + Number(secondsInput)
-  
+  timepass=totalSeconds1
+  timerInterval = setInterval(decrementTime, 1000)
+}
+function startTimer1() {
+  stopt = false
+  totalSeconds1 = timepass
+  timepass=totalSeconds1
   timerInterval = setInterval(decrementTime, 1000)
   
 }
 
-
 function stopTimer() {
+  stopt = true
   clearInterval(timerInterval)
 }
 
@@ -31,7 +42,14 @@ function decrementTime() {
     let hours = Math.floor(totalSeconds1 / 3600)
     let minutes = Math.floor((totalSeconds1 - (hours * 3600)) / 60)
     let seconds = totalSeconds1 - (hours * 3600) - (minutes * 60)
+    timepass -=1
     let timeString = hours.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0")
     document.getElementById("time").innerHTML = timeString
+  }
+}
+
+function conTimer() {
+  if (stopt === true) {
+    startTimer1()
   }
 }
